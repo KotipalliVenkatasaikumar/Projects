b55c294dd4edc2178383230f82ed401ac5767be4
KotipalliVenkatasaikumar <saikumar04899@gmail.com>
2023-11-20 09:24:13 +0530

    corenuts completed

---
+++ Corenuts/src/main/java/com/sai/Corenuts/Demo.java
@@ -0,0 +1,16 @@
+package com.sai.Corenuts;
+
+import java.util.Scanner;
+
+public class Demo {
+	public static void main(String[] args) {
+		Scanner scan =new Scanner(System.in);
+		System.out.println("enter first num");
+		int num1 =scan.nextInt();
+		System.out.println("second num");
+		int num2=scan.nextInt();
+		
+		System.out.println(num1+num2);
+	}
+
+}
